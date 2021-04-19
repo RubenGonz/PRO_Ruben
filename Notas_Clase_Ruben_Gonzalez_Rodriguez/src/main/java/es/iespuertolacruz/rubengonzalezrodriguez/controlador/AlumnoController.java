8c43a0f3c6decd4f348d7ea192c338c2ebfd3953
RubenGonz <ruben30303030@gmail.com>
2021-04-19 16:40:27 +0100

    Cambios en algunas clases y creacion de nuevas

--- Notas_Clase_Ruben_Gonzalez_Rodriguez/src/main/java/es/iespuertolacruz/rubengonzalezrodriguez/controlador/AlumnoController.java
+++ Notas_Clase_Ruben_Gonzalez_Rodriguez/src/main/java/es/iespuertolacruz/rubengonzalezrodriguez/controlador/AlumnoController.java
@@ -19,11 +19,11 @@ public class AlumnoController {
 
     /**
      * Metodo que valida si un alumno es valido o no
+     * 
      * @param alumno a validar
-     * @return true si es valido o una excepcion con mensaje si no lo es
      * @throws AlumnoException excepcion propia
      */
-    public boolean validarAlumno(Alumno alumno) throws AlumnoException {
+    public void validarAlumno(Alumno alumno) throws AlumnoException {
         String mensaje = "";
 
         if (alumno == null) {
@@ -50,8 +50,77 @@ public class AlumnoController {
         if (!mensaje.isEmpty()) {
             throw new AlumnoException(mensaje);
         }
+    }
 
-        return false;
+    /**
+     * Metodo encargado de insertar una 
+     * 
+     * @param fruta a insertar
+     * @throws FrutaException con mensaje controlado
+     */
+    public void insertar(Fruta fruta) throws FrutaException {
+        validarFruta(fruta);
+        if (existe(fruta)) {
+            throw new FrutaException("La fruta indicada ya existe");
+        }
+        frutaModelo.insertar(fruta);
+    }
+
+    /**
+     * Metodo encargado de eliminar
+     * 
+     * @param fruta a eliminar
+     * @throws FrutaException
+     */
+    public void eliminar(Fruta fruta) throws FrutaException {
+        validarFruta(fruta);
+        if (!existe(fruta)) {
+            throw new FrutaException("La fruta indicada NO existe");
+        }
+        frutaModelo.eliminar(fruta);
+    }
+
+    public void eliminar(String identificador) throws FrutaException {
+        Fruta fruta;
+        fruta = buscar(identificador);
+        if (fruta == null) {
+            throw new FrutaException("La fruta indicada NO existe");
+        }
+        frutaModelo.eliminar(fruta);
+    }
+
+    /**
+     * Metodo encargado de buscar por identificador
+     * 
+     * @param identificador para localizar la fruta
+     * @return fruta a traves del identificador
+     */
+    public Fruta buscar(String identificador) {
+        Fruta fruta = null;
+        fruta = frutaModelo.buscar(identificador);
+        return fruta;
+    }
+
+    public void modificar(Fruta fruta) {
+        // lo que sea
+    }
+
+    /**
+     * Funcion encargada de verificar si existe la fruta
+     * 
+     * @param fruta a encontrar
+     * @return true/false
+     */
+    private boolean existe(Fruta fruta) {
+        boolean encontrada = false;
+        Fruta frutaEncontrada;
+
+        frutaEncontrada = buscar(fruta.getIdentificador());
+        if (frutaEncontrada != null) {
+            encontrada = true;
+        }
+
+        return encontrada;
     }
 
 }
