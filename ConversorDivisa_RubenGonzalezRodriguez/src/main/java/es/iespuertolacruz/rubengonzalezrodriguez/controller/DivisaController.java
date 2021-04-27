98e257d097883aeb4205379ae75abb3d5486c347
RubenGonz <ruben30303030@gmail.com>
2021-04-27 13:31:55 +0100

    Avances en la practica del conversor de divisa

--- ConversorDivisa_RubenGonzalezRodriguez/src/main/java/es/iespuertolacruz/rubengonzalezrodriguez/controller/DivisaController.java
+++ ConversorDivisa_RubenGonzalezRodriguez/src/main/java/es/iespuertolacruz/rubengonzalezrodriguez/controller/DivisaController.java
@@ -9,11 +9,11 @@ import es.iespuertolacruz.rubengonzalezrodriguez.modelo.DivisaModelo;
  */
 public class DivisaController {
 
-    //Variables de clase
+    // Variables de clase
 
     DivisaModelo divisaModelo;
 
-    //Constructores
+    // Constructores
 
     /**
      * Constructor por defecto
@@ -22,17 +22,18 @@ public class DivisaController {
         divisaModelo = new DivisaModelo();
     }
 
-    //Metodos y funciones
-    
+    // Metodos y funciones
+
     /**
      * Metodo que valida si una divisa dada es valida
+     * 
      * @param divisa a comprobar
      * @throws DivisaException es nuestra excepcion propia
      */
     public void validarDivisa(Divisa divisa) throws DivisaException {
         String mensaje = "";
 
-        if(divisa == null) {
+        if (divisa == null) {
             mensaje = "Se esta intentando validar una divisa que es nula o vacia";
             throw new DivisaException(mensaje);
         }
@@ -40,7 +41,7 @@ public class DivisaController {
         if (divisa.getCodigo() == null || divisa.getCodigo().isEmpty()) {
             mensaje += "Se esta intentando validar una divisa con codigo es nulo o vacio";
         }
-    
+
         if (divisa.getValor() < 0) {
             mensaje += "Se esta intentando validar una divisa con valor 0 o menor";
         }
@@ -48,6 +49,101 @@ public class DivisaController {
         if (!mensaje.isEmpty()) {
             throw new DivisaException(mensaje);
         }
+    }
+
+    /**
+     * Metodo que inserta una divisa comprobando si es valida
+     * 
+     * @param divisa a insertar
+     * @throws DivisaException excepcion propia
+     */
+    public void insertar(Divisa divisa) throws DivisaException {
+        validarDivisa(divisa);
+        if (existe(divisa)) {
+            throw new DivisaException("La divisa indicada ya existe");
+        }
+        divisaModelo.insertar(divisa);
+    }
+
+    /**
+     * Metodo que elimina una divisa
+     * 
+     * @param divisa a eliminar
+     * @throws DivisaException excepcion propia
+     */
+    public void eliminar(Divisa divisa) throws DivisaException {
+        if (!existe(divisa)) {
+            throw new DivisaException("La divisa indicada no existe");
+        }
+        divisaModelo.eliminar(divisa);
+    }
+
+    /**
+     * Funcion encargada de buscar una divisa por su codigo
+     * 
+     * @param codDivisa a buscar
+     * @return la divisa encontrada o null
+     */
+    public Divisa buscar(String codDivisa) {
+        Divisa divisa = null;
+        divisa = divisaModelo.buscar(codDivisa);
+        return divisa;
+    }
+
+    /**
+     * Funcion encargada de comprobar si una divisa existe ya
+     * 
+     * @param divisa a comprobar
+     * @return true si existe o false si no existe
+     */
+    public boolean existe(Divisa divisa) {
+        boolean encontrada = false;
+        Divisa divisaEncontrada;
+
+        divisaEncontrada = buscar(divisa.getCodigo());
+        if (divisaEncontrada != null) {
+            encontrada = true;
+        }
+        return encontrada;
+    }
+
+    /**
+     * Funcion que devuelve el valor de una divisa
+     * 
+     * @param codDivisa a obtener
+     * @return la divisa
+     * @throws DivisaException excepcion propia
+     */
+    public Divisa obtener(String codDivisa) throws DivisaException {
+        try {
+            return divisaModelo.obtener(codDivisa);
+        } catch (DivisaException e) {
+            throw new DivisaException("No se ha podido obtener la divisa");
+        }
+    }
+
+    /**
+     * Funcion que te convierte el valor de una divisa a otra
+     * 
+     * @param codDivisaOrigen  codigo de la divisa de origen
+     * @param codDivisaDestino codigo de la divisa de destino
+     * @param importe          valor que se quiere convertir
+     * @return valor al cambio
+     * @throws DivisaException excepcion propia
+     */
+    public float convertir(String codDivisaOrigen, String codDivisaDestino, float importe) throws DivisaException {
+        try {
+            return divisaModelo.convertir(codDivisaOrigen, codDivisaDestino, importe);
+        } catch (DivisaException exception) {
+            throw new DivisaException("No se ha podido convertir la divisa");
+        }
+    }
+
+    /**
+     * Metodo toString
+     */
+    public String toString() {
+        return divisaModelo.toString();
     }
 
 }
--- ConversorDivisa_RubenGonzalezRodriguez/src/main/java/es/iespuertolacruz/rubengonzalezrodriguez/modelo/DivisaModelo.java
+++ ConversorDivisa_RubenGonzalezRodriguez/src/main/java/es/iespuertolacruz/rubengonzalezrodriguez/modelo/DivisaModelo.java
@@ -3,17 +3,18 @@ package es.iespuertolacruz.rubengonzalezrodriguez.modelo;
 import java.util.HashMap;
 
 import es.iespuertolacruz.rubengonzalezrodriguez.api.Divisa;
+import es.iespuertolacruz.rubengonzalezrodriguez.exception.DivisaException;
 
 /**
  * Clase que guarda la informacion de las divisas y la usa
  */
 public class DivisaModelo {
-    
-    //Variables de clase
 
-    HashMap<String,Divisa> listaDivisas;
+    // Variables de clase
 
-    //Constructores
+    HashMap<String, Divisa> listaDivisas;
+
+    // Constructores
 
     /**
      * Constructor por defecto de DivisaModelo
@@ -22,32 +23,106 @@ public class DivisaModelo {
         listaDivisas = new HashMap<>();
     }
 
-    //Metodos y funciones
+    // Metodos y funciones
 
-    public void insertar() {
-        
+    /**
+     * Metodo que inserta una divisa si no existe
+     * 
+     * @param divisa a insertar
+     * @throws DivisaException excepcion propia
+     */
+    public void insertar(Divisa divisa) throws DivisaException {
+        if (existe(divisa)) {
+            throw new DivisaException("La divisa que intenta insertar en la lista ya esta insertada");
+        }
+        listaDivisas.put(divisa.getCodigo(), divisa);
     }
 
-    public void eliminar() {
-        
+    /**
+     * Metodo que elimina una divisa de la lista si existiese
+     * 
+     * @param divisa a eliminar
+     * @throws DivisaException excepcion propia
+     */
+    public void eliminar(Divisa divisa) throws DivisaException {
+        if (!existe(divisa)) {
+            throw new DivisaException("La divisa que intenta eliminar de la lista no esta insertada");
+        }
+        listaDivisas.remove(divisa.getCodigo(), divisa);
     }
 
-    public void existe() {
-        
+    /**
+     * Funcion que pasado un codigo te busca una divisa
+     * 
+     * @param codDivisa codigo de la divisa a buscar
+     * @return la divisa a buscar o null
+     */
+    public Divisa buscar(String codDivisa) {
+        Divisa divisa = null;
+        divisa = listaDivisas.get(codDivisa);
+        return divisa;
     }
 
-    public void buscar() {
-        
+    /**
+     * Funcion que averigua si la divisa existe en la lista
+     * 
+     * @param divisa a comprobar
+     * @return true si existe o false si no existe
+     */
+    public boolean existe(Divisa divisa) {
+        Divisa divisaAencontrar;
+        divisaAencontrar = buscar(divisa.getCodigo());
+        if (divisaAencontrar != null) {
+            return true;
+        }
+        return false;
     }
 
-    public void obtener() {
-        
+    /**
+     * Funcion que devuelve el valor de una divisa
+     * 
+     * @param codDivisa a obtener
+     * @return la divisa
+     * @throws DivisaException excepcion propia
+     */
+    public Divisa obtener(String codDivisa) throws DivisaException {
+        Divisa divisa = null;
+        divisa = buscar(codDivisa);
+        if (divisa == null) {
+            throw new DivisaException("La divisa que se quiere obtener no existe");
+        }
+        return divisa;
     }
 
-    public void convertir(Divisa divisaOrigen) {
-        
+    /**
+     * Funcion que te convierte el valor de una divisa a otra
+     * 
+     * @param codDivisaOrigen  codigo de la divisa de origen
+     * @param codDivisaDestino codigo de la divisa de destino
+     * @param importe          valor que se quiere convertir
+     * @return valor al cambio
+     * @throws DivisaException excepcion propia
+     */
+    public float convertir(String codDivisaOrigen, String codDivisaDestino, float importe) throws DivisaException {
+        float valorDividaOrigen;
+        float valorDividaDestino;
+        try {
+            valorDividaOrigen = obtener(codDivisaOrigen).getValor();
+        } catch (DivisaException exception) {
+            throw new DivisaException("No se le pudo dar valor a a la variable");
+        }
+        try {
+            valorDividaDestino = obtener(codDivisaDestino).getValor();
+        } catch (DivisaException exception) {
+            throw new DivisaException("No se le pudo dar valor a a la variable");
+        }
+        float euros = importe / valorDividaOrigen;
+        return euros * valorDividaDestino;
     }
 
+    /**
+     * Metodo toString de DivisaModelo
+     */
     public String toString() {
         return listaDivisas.toString();
     }
